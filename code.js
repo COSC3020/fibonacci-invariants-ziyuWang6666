fb7a9e8d460670d94be0e4b0398f0a6be0ce9d32
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-09-18 10:40:56 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
-function fib(n) {
-    return n;
+function fib(s) {
+    if(s == 0) return [0];
+    if(s < 2) {
+        return [0,1];
+    }
+    if(s < 3) {
+        return [0,1,1];
+    } 
+    else {
+      
+        var resultArr = fib(s-1);
+        resultArr.push(resultArr[s-2]+resultArr[s-1]);
+        return resultArr;
+    }
 }
+console.log(fib(10));
+// get help from TA
